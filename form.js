13fe553526413ff7b2eaed4dfae8fcff0d6dc0a1
karunyashiva <50661970+karunyashiva@users.noreply.github.com>
2019-09-24 12:15:14 +0530

    Date : 24-09-2019

---
+++ form.js
@@ -0,0 +1,6 @@
+function myFunc(){
+    document.getElementById('main').innerHTML="Registered Sucessfully!!!!!!";
+    document.getElementById('head').style.display="none";
+    document.getElementById('main').style.fontSize="30px";
+    document.getElementById('main').style.padding="10%";
+}
